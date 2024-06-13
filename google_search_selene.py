0731c4301f095cb4b2a5ca7ea98edde1a5b65d8b
Azamat Abdurashidov <royalty5611@gmail.com>
2024-06-13 10:42:23 +0300

    google search

---
+++ google_search_selene.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser, be, have
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    yield
+    browser.quit()
+
+
+def test_search_selene():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+
